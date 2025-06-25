ba27c1be51a25579c58a87dfb3319ab979f63363
Motion <benjaminokafor2007@gmail.com>
2025-06-25 23:08:09 +0100

    realme

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,12 +1,5 @@
-'use client';
 
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
-import Sidebar from '@/components/SideBar/SideBar';
-import { motion } from 'framer-motion';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { getUser } from '@/lib/getUser';
 import Dashboard from './Dashboard';
 
