0e17d43e21f35f061a7b574d60d9ee52fa075e4b
Motion <benjaminokafor2007@gmail.com>
2025-06-25 15:15:31 +0100

    latest

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDB } from "@/lib/connectDB";
 import User from "@/models/User";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 export const authOptions = {
   providers: [
